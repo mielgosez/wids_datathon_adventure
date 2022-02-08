2cb63917042ff40ccb067ac3dbc9df2c4f78a184
Ferran Martí <ferran.marti@MacBook-MacBook-Pro-de-Administrador.local>
2022-02-08 12:27:03 +0100

    Modify train_model script

--- train_model.py
+++ train_model.py
@@ -10,6 +10,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split, GridSearchCV
+from xgboost import XGBRegressor
 # Custom
 from data.metadata import *
 
@@ -31,6 +32,10 @@ def extract_data():
 
 def create_preprocessing_numeric_pipeline(model_name: str):
     if model_name == 'random_forest':
+        numeric_transformer = Pipeline(
+            steps=[("imputer", SimpleImputer(strategy="median"))]
+        )
+    elif model_name == 'XGBoost':
         numeric_transformer = Pipeline(
             steps=[("imputer", SimpleImputer(strategy="median"))]
         )
@@ -58,19 +63,35 @@ def create_pipeline_random_forest():
     return reg
 
 
-def train_model():
+def create_pipeline_XGBoost():
+    preprocessor_pipeline = create_preprocessing_pipeline(model_name='XGBoost')
+    reg = Pipeline(
+        steps=[("preprocessor", preprocessor_pipeline), ("regressor", XGBRegressor())]
+    )
+    return reg
+
+
+def train_model(dt_train_original: pd.DataFrame, model_name:str):
+    df = dt_train_original.copy()
     local_logger.info('Train: Preparing data')
-    df = pd.read_csv(train_data_path)
     y = df[target_var]
     df.drop(target_var, axis=1, inplace=True)
-    df.drop('facility_type', axis=1, inplace=True)
+    df.drop(['facility_type','id'], axis=1, inplace=True)
     X = df
     local_logger.info('Creating pipeline')
-    reg = create_pipeline_random_forest()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    reg.fit(X_train, y_train)
-    param_grid = {
-        "regressor__n_estimators": [10, 50, 100],
-    }
-    grid_search = GridSearchCV(reg, param_grid, cv=10)
-    return grid_search.best_params_
+    if model_name == 'random_forest':
+        reg = create_pipeline_random_forest()
+        param_grid = {
+            "regressor__n_estimators": [10, 50, 100],
+        }
+    elif model_name == 'XGBoost':
+        reg = create_pipeline_XGBoost()
+        param_grid = {'max_depth': [3, 6, 10],
+                  'learning_rate': [0.01, 0.05, 0.1],
+                  'n_estimators': [50, 100]}
+    else:
+        raise NotImplementedError(f'{model_name} has not been implemented')
+    grid_search = GridSearchCV(reg, param_grid, cv=10, scoring='neg_root_mean_squared_error')
+    grid_search.fit(X_train, y_train)
+    return grid_search
